87185a8d540400de16f3f913ab54a8d228225dae
Ben Mathews <benjamin.l.mathews@gmail.com>
2015-12-07 22:07:35 -0800

    Updating ReceivingActivity to handle incoming data.

--- app/src/main/java/android/integratedapps/androidsensorframework/ReceivingActivity.java
+++ app/src/main/java/android/integratedapps/androidsensorframework/ReceivingActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ReceivingActivity extends Activity {
 
@@ -17,8 +18,8 @@ public class ReceivingActivity extends Activity {
         String type = intent.getType();
         if (Intent.ACTION_SEND.equals(action) && type != null) {
             if ("text/plain".equals(type)) {
-                String string1 = intent.getExtras().getString(Intent.EXTRA_TEXT);
-                String string2 = intent.getExtras().getString("MyKey");
+                String MessageText = intent.getExtras().getString(Intent.EXTRA_TEXT);
+                Log.println(1, "Debug", MessageText);
             } else {
                 //
             }
